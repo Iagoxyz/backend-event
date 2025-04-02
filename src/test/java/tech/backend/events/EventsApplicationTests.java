25331d518f36726278028049d5aa055f1dd92fdc
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-04-02 20:20:24 -0300

    Project created

---
+++ src/test/java/tech/backend/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.backend.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
