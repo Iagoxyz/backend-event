54f09fa4f2edca9758288f187c1bc9846d3b7ad0
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-04-09 21:45:53 -0300

    Implement SubscriptionService

---
+++ src/main/java/tech/backend/events/service/SubscriptionService.java
@@ -0,0 +1,14 @@
+package tech.backend.events.service;
+
+import org.springframework.stereotype.Service;
+import tech.backend.events.model.Subscription;
+
+@Service
+public class SubscriptionService {
+
+    public Subscription createNewSubscription(String eventName, String User user) {
+        Subscription subs = new Subscription();
+
+
+    }
+}
